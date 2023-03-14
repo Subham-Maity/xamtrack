d02a786e22974a8ab148bf3efe9832efc053a946
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-03-14 23:41:16 +0530

    Basic Controller Setup (Task.js)

--- controllers/task.js
+++ controllers/task.js
@@ -1,2 +1,13 @@
 
+const getAllTasks = async(req, res) => {
+    res.status(200).json({ msg: 'Show all tasks' });
+  };
 
+const getAllTasksTesting = async(req, res) => {
+    res.status(200).json({ msg: 'Show all tasks testing' });
+  };
+
+module.exports = {
+    getAllTasks,
+    getAllTasksTesting
+  };
