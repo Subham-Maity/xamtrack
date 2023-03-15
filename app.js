df7b3fb6bedb25fc3fbd207915b51003105cb4a2
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-03-15 23:18:48 +0530

    .env setup (secure this) 🔑
    FakeDemoENV added (.txt format)

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use("/api/tasks" , tasks_routes);
 // Async Function
 const start = async () => {
   try {
-    await connectDB();
+    await connectDB(process.env.MONGODB_URL);
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
-uri = "mongodb+srv://dummyuser:dummyuser123@xamtrack.xknpt1w.mongodb.net/xamtrack?retryWrites=true&w=majority"
 
-const connectDB = () => { 
+const connectDB = (uri) => { 
     // console.log("Connecting to DB");
     return mongoose.connect(uri, 
     { 
