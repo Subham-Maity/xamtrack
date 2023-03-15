3d6ded999fef6d29a13dec16994d9eb4d0017cc4
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-03-15 21:14:57 +0530

    Fix bug (uri) 🪲
    passing the string ‘uri’ instead of the variable uri to the mongoose.connect function. This is why I'm getting the invalid scheme error.

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 uri = "mongodb+srv://dummyuser:dummyuser123@xamtrack.xknpt1w.mongodb.net/xamtrack?retryWrites=true&w=majority"
 
 const connectDB = () => { 
-    return mongoose.connect('uri', 
+    return mongoose.connect(uri, 
     { 
         useNewUrlParser: true, 
         useUnifiedTopology: true 
