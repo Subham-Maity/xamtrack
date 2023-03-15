0a87b918284248d196b53a0ad5d0b118a4e54d18
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-03-15 21:22:08 +0530

    Remove the line ⚡

--- db/connect.js
+++ db/connect.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 uri = "mongodb+srv://dummyuser:dummyuser123@xamtrack.xknpt1w.mongodb.net/xamtrack?retryWrites=true&w=majority"
 
 const connectDB = () => { 
+    // console.log("Connecting to DB");
     return mongoose.connect(uri, 
     { 
         useNewUrlParser: true, 
