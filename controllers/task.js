3d1926a38ac07e82d14ac7d393072baa177fc472
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-03-16 20:19:27 +0530

    Add Filtration & Searching Functionality with Query Props ▶️

--- controllers/task.js
+++ controllers/task.js
@@ -1,13 +1,16 @@
 const Task = require('../models/Task');
 
 const getAllTasks = async(req, res) => {
-   const myData = await Task.find({});
+  const myData = await Task.find(req.query);
 
     res.status(200).json({ myData });
   };
 
 const getAllTasksTesting = async(req, res) => {
-    const myData = await Task.find({status: "incomplete"});
+    const myData = await Task.find(req.query);
+
+    console.log("User searched for: ", req.query);
+
     res.status(200).json({ myData });
   };
 
