e36e2fdd21554ce31a68fe54e95887c2f51bc165
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-03-16 22:40:08 +0530

    Improve Filter in API & Make API Work Better

--- controllers/task.js
+++ controllers/task.js
@@ -1,15 +1,22 @@
 const Task = require('../models/Task');
 
 const getAllTasks = async(req, res) => {
-  const myData = await Task.find(req.query);
+
+   const {priority} = req.query;
+   const queryObject = { };
+
+   if(priority) {
+        queryObject.priority = priority;
+        console.log(queryObject.priority);
+    }
+
+  const myData = await Task.find(queryObject);
 
     res.status(200).json({ myData });
   };
 
 const getAllTasksTesting = async(req, res) => {
     const myData = await Task.find(req.query);
-
-    console.log("User searched for: ", req.query);
 
     res.status(200).json({ myData });
   };
