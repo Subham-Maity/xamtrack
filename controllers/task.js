9d2722e3f5852fdc90d91952c92e3710c60951d1
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-03-18 18:58:37 +0530

    Handle Status Query

--- controllers/task.js
+++ controllers/task.js
@@ -1,27 +1,31 @@
-const Task = require('../models/Task');
+const Task = require("../models/Task");
 
-const getAllTasks = async(req, res) => {
+const getAllTasks = async (req, res) => {
+  const { priority, status } = req.query;
+  const queryObject = {};
 
-   const {priority} = req.query;
-   const queryObject = { };
+  if (priority) {
+    queryObject.priority = priority;
+  }
 
-   if(priority) {
-        queryObject.priority = priority;
-        console.log(queryObject.priority);
-    }
+  if (status) {
+    queryObject.status = status;
+  }
+
+  console.log(queryObject);
 
   const myData = await Task.find(queryObject);
 
-    res.status(200).json({ myData });
-  };
+  res.status(200).json({ myData });
+};
 
-const getAllTasksTesting = async(req, res) => {
-    const myData = await Task.find(req.query);
+const getAllTasksTesting = async (req, res) => {
+  const myData = await Task.find(req.query);
 
-    res.status(200).json({ myData });
-  };
+  res.status(200).json({ myData });
+};
 
 module.exports = {
-    getAllTasks,
-    getAllTasksTesting
-  };
+  getAllTasks,
+  getAllTasksTesting,
+};
