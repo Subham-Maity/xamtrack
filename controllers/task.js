09a0e6c8fdf01fb0ae3ad8a586ddbd209d34c3a3
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-03-19 00:12:46 +0530

    Try to implement sort function (problem)

--- controllers/task.js
+++ controllers/task.js
@@ -1,7 +1,7 @@
 const Task = require("../models/Task");
 
 const getAllTasks = async (req, res) => {
-  const { priority, status, description } = req.query;
+  const { priority, status, description, sort } = req.query;
   const queryObject = {};
 
   if (priority) {
@@ -16,15 +16,27 @@ const getAllTasks = async (req, res) => {
     queryObject.description = { $regex: description, $options: "i" };
   }
 
+  let apiData =Task.find(queryObject);
+
+  if (sort) {
+    let sortProblem = sort.replace(",", " ");
+    apiData = apiData.sort(sortProblem);
+  }
+
+
+
+
+
+
   console.log(queryObject);
 
-  const myData = await Task.find(queryObject);
+  const myData = await apiData;
 
   res.status(200).json({ myData });
 };
 
 const getAllTasksTesting = async (req, res) => {
-  const myData = await Task.find(req.query);
+  const myData = await Task.find(req.query).sort('-dueDate');
 
   res.status(200).json({ myData });
 };
